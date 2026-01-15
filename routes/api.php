a5045959aee92f5e332b3b84ef9ceb9b0de7e533
kiranv04 <kiranv04@gmail.com>
2026-01-15 14:43:47 +0530

    Feat: States - Added StateController. - Linked it with LocationController

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\LocationController;
+use App\Http\Controllers\Api\StateController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,4 +13,5 @@ Route::middleware('auth:sanctum')->group(function () {
 //    Route::post('/change-password', [AuthController::class, 'changePassword']);
    Route::apiResource('locations', LocationController::class);
    Route::post('/locations/{id}/activate', [LocationController::class, 'actrivate']);
+   Route::get('/states', [StateController::class, 'index']);
 });
